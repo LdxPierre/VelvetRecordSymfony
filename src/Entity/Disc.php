9d080efc1fc714d24631fbcf4e894f0c66caf5f3
Pierre <pierre.ledoux@outlook.fr>
2023-04-11 11:08:16 +0200

    constraints

--- src/Entity/Disc.php
+++ src/Entity/Disc.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\DiscRepository;
+use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: DiscRepository::class)]
@@ -13,22 +14,81 @@ class Disc
     #[ORM\Column]
     private ?int $id = null;
 
+    #[Assert\NotBlank(
+        message: 'Veuillez saisir un titre.',
+        )]
+    #[Assert\Length(
+        min: 2,
+        max: 50,
+        minMessage: 'Le titre doit comporter entre 1 et 50 caractères.',
+        maxMessage: 'Le titre doit comporter entre 1 et 50 caractères.',
+        )]
+    #[Assert\Regex(
+        pattern: '/^[a-zA-Z\d &é\-èçà@+=%?,!]{1,50}$/',
+        message: 'Le titre contient des caractères invalides.',
+    )]
     #[ORM\Column(length: 255)]
     private ?string $title = null;
 
+    #[Assert\NotBlank(
+        message: 'Veuillez saisir une année.',
+    )]
+    #[Assert\Length(
+        min: 4,
+        max: 4,
+        minMessage: 'L\'année doit contenir 4 chiffres',
+        maxMessage: 'L\'année doit contenir 4 chiffres',
+    )]
+    #[Assert\Regex(
+        pattern: '/^[0123456789]{4}$/',
+        message: 'L`année doit contenir 4 chiffres',
+    )]
     #[ORM\Column(length: 4, nullable: true)]
     private ?string $year = null;
 
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $picture = null;
 
+    #[Assert\NotBlank(
+        message: 'Veuillez saisir un label.',
+        )]
+    #[Assert\Length(
+        min:2,
+        max:50,
+        minMessage: 'Le label doit comporter entre 2 et 4 caractères.',
+        maxMessage: 'Le label doit comporter entre 2 et 4 caractères.',
+    )]
+    #[Assert\Regex(
+        pattern: '/^[a-zA-Z\d&é\-èçà@+=%?,! ]{1,50}$/',
+        message: 'Le label contient des caractères invalide.',
+    )]
     #[ORM\Column(length: 255)]
     private ?string $label = null;
 
+    #[Assert\NotBlank(
+        message: 'Veuillez saisir un genre.',
+        )]
+    #[Assert\Length(
+        min:2,
+        max:50,
+        minMessage: 'Le genre doit comporter entre 2 et 4 caractères.',
+        maxMessage: 'Le genre doit comporter entre 2 et 4 caractères.',
+    )]
+    #[Assert\Regex(
+        pattern: '/^[a-zA-Z\d\/&é\-èçà@+=%?,! ]{1,50}$/',
+        message: 'Le genre contient des caractères invalide.',
+    )]
     #[ORM\Column(length: 255)]
     private ?string $genre = null;
 
+    #[Assert\NotBlank(
+        message: 'Veuillez saisir un prix.',
+        )]
     #[ORM\Column]
+    #[Assert\Type(
+        type: 'int',
+        message: 'Veuillez saisir un nombre.',
+    )]
     private ?int $price = null;
 
     #[ORM\ManyToOne(inversedBy: 'discs')]
